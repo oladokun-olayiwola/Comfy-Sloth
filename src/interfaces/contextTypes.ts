0fb31f3bd412b9b0259a01301ae726c6681baef6
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2025-02-20 00:44:14 +0100

    feat: add types for the context

---
+++ src/interfaces/contextTypes.ts
@@ -0,0 +1,78 @@
+import { ReactNode } from "react";
+import { Product } from "./reducerTypes";
+
+// reducerTypes.ts
+export interface CartItem {
+  id: string;
+  name: string;
+  color: string;
+  amount: number;
+  image: string;
+  price: number;
+  max: number;
+}
+
+export interface CartState {
+  cart: CartItem[];
+  total_items: number;
+  total_amount: number;
+  shipping_fee: number;
+}
+
+export interface CartProviderProps {
+  children: ReactNode;
+}
+
+export type CartAction =
+  | {
+      type: 'ADD_TO_CART';
+      payload: { id: string; color: string; amount: number; product: any };
+    }
+  | { type: 'REMOVE_CART_ITEM'; payload: string }
+  | {
+      type: 'TOGGLE_CART_ITEM_AMOUNT';
+      payload: { id: string; value: 'inc' | 'dec' };
+    }
+  | { type: 'CLEAR_CART' }
+  | { type: 'COUNT_CART_TOTALS' };
+
+export interface CartContextValue extends CartState {
+  addToCart: (id: string, color: string, amount: number, product: any) => void;
+  removeItem: (id: string) => void;
+  toggleAmount: (id: string, value: 'inc' | 'dec') => void;
+  clearCart: () => void;
+}
+
+
+// Filter Context
+
+interface Filters {
+  text: string;
+  company: string;
+  category: string;
+  color: string;
+  min_price: number;
+  max_price: number;
+  price: number;
+  shipping: boolean;
+}
+
+export interface FilterState {
+  filtered_products: Product[];
+  all_products: Product[];
+  grid_view: boolean;
+  sort: string;
+  filters: Filters;
+}
+
+export interface FilterProviderProps {
+  children: ReactNode;
+}
+
+export interface FilterContextValue extends FilterState {
+  setGridView: () => void;
+  setListView: () => void;
+  updateSort: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  updateFilters: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  clearFilters: () => void;
+}
