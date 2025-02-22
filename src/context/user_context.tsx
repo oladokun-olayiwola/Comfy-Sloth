934e36cf69279494ec7b63a0d50ef0ee5412a6d4
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2025-02-22 17:14:07 +0100

    chore: convert user context to typescript

---
+++ src/context/user_context.tsx
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { useAuth0, User } from '@auth0/auth0-react'
+import { UserContextValue, UserProviderProps } from '../interfaces/contextTypes'
+
+const UserContext = createContext<UserContextValue | null>(null)
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  const { loginWithRedirect, logout, user } = useAuth0()
+
+  const [myUser, setMyUser] = useState<User | undefined>(undefined)
+
+  useEffect(() => {
+    setMyUser(user)
+  }, [user])
+
+  return (
+    <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+// make sure use
+export const useUserContext = () => {
+  return useContext(UserContext)
+}
