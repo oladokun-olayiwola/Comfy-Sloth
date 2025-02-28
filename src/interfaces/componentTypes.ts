4b6feea0f964d68106860213f12dab33ad28207a
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2025-02-28 16:09:37 +0100

    feat: add types for components

--- src/interfaces/componentTypes.ts
+++ src/interfaces/componentTypes.ts
@@ -1,6 +1,49 @@
 // Auth Wrapper Types
 import { ReactNode } from "react";
+import { Product } from "./reducerTypes";
 
 export interface ComponentProps {
   children: ReactNode;
+}
+
+export interface PageHeroProps {
+  title: string;
+  product?: Product;
+}
+
+export interface CartItemProps {
+  id: string;
+  image: string;
+  name: string;
+  color: string;
+  price: number;
+  amount: number;
+}
+
+export interface ListViewProps {
+  products: Product[];
+}
+ export interface ProductImage {
+  url: string;
+  filename?: string;
+}
+
+export interface ProductImagesProp {
+  images: Array<ProductImage>
+}
+export interface ProductProps {
+  image: ProductImage;
+  name: string;
+  price: number;
+  id: string;
+}
+
+export interface GridViewProps {
+  products: Product[]
+}
+
+export interface AmountButtonsProps {
+  increase: number;
+  decrease: number;
+  amount: number;
 }
