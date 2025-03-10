ccb4ed3a34b153120723738ce769710b27846d10
Olayiwola <52329972+oladokun-olayiwola@users.noreply.github.com>
2025-03-10 19:59:39 +0100

    chore: update private route component

--- src/pages/PrivateRoute.tsx
+++ src/pages/PrivateRoute.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-// import { useUserContext } from '../context/user_context'
-import { useAuth0 } from '@auth0/auth0-react'
-import { ComponentProps } from '../interfaces/componentTypes'
+import React, { ReactNode } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
-const PrivateRoute: React.FC<ComponentProps> = ({ children }) => {
-  const { user } = useAuth0()
-  if(!user){
-    return <Navigate to="/" />
+const PrivateRoute: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const { user } = useAuth0();
+  
+  if (!user) {
+    return <Navigate to="/" />;
   }
-  return children;
-}
-export default PrivateRoute
+  
+  return <>{children}</>;
+};
+
+export default PrivateRoute;
